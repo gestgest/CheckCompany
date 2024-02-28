662161e1dd79b06da03a7a4d0bc774c83d9beb7b
gestgest <milkan660@naver.com>
2024-02-28 15:03:44 +0900

    윈도우 스위칭

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -7,9 +7,15 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI moneyText;
-
+    [SerializeField] private Window [] windows;
     public void SetMoneyText(int value)
     {
         moneyText.text = value.ToString();
+    }
+
+    public void SwitchingWindow(int index)
+    {
+        bool isActive = windows[index].gameObject.activeSelf;
+        windows[index].gameObject.SetActive(!isActive);
     }
 }
