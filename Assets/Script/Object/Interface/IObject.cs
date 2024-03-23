6c5562733fb938c21df7551e6de653f1a8629002
gestgest <87427692+gestgest@users.noreply.github.com>
2024-03-23 23:02:17 +0900

    add ShopWindow

---
+++ Assets/Script/Object/Interface/IObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//그 타일맵에서 놓는 물체
+interface IObject
+{
+}
---
+++ Assets/Script/ScriptableObject/ObjectSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//놓는 물체
+[CreateAssetMenu(fileName = "ObjectSO", menuName = "ScriptableObject/ObjectSO")]
+public class ObjectSO : ScriptableObject
+{
+    [SerializeField] private string object_name;
+    [SerializeField] private int width;
+    [SerializeField] private int length;
+    [SerializeField] private int moeny;
+    //fbx?
+    //type [책상인지 뭔지]
+}
