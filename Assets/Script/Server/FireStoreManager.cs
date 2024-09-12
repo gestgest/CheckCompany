2a048ead135a56fcf5a7915613349b47f5f11609
gestgest <87427692+gestgest@users.noreply.github.com>
2024-09-12 23:26:36 +0900

    외부 package gitignore 정리

---
+++ Assets/Script/Server/FireStoreManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FireStoreManager : MonoBehaviour
+{
+}
+//mDatabaseRef.Child("users").Child(userId).Child("username").SetValueAsync(name);
