3e4ae61a661a19254ef1981530dc465f68d17e25
gestgest <87427692+gestgest@users.noreply.github.com>
2024-09-14 00:51:43 +0900

    set만 됨. get도 되기는 하는데...

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] UIManager ui_manager;
+    [SerializeField] FireStoreManager fireStoreManager;
+    
     //int executive = 1; //임원수
     int money;
     //Reputation reputation = Reputation.single; //레벨 [명예]
@@ -12,16 +15,29 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        //디버깅
-        Money = 0;
+        fireStoreManager.Init();
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("시작");
+            Task.Run(() => fireStoreManager.GetFirestore("milkan660", "money"));
+
+            //Money = (int) fireStoreManager.GetFirestore("milkan660", "money");
+            
+        }
     }
 
     public int Money {  
         //애초에 서버에 데이터를 넣는 게 낫지 않나
         get { return money; } 
         set { 
+            Debug.Log("들어간닷 : " + value);
             money = value;
-            
+            fireStoreManager.SetFirestore("money", money);
             //서버 로딩
             ui_manager.SetMoneyText(value);
         } 
--- Assets/Script/Server/FireStoreManager.cs
+++ Assets/Script/Server/FireStoreManager.cs
@@ -1,10 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Firestore;
+using Firebase.Extensions;
+using System.Threading.Tasks;
 
 
 
 public class FireStoreManager : MonoBehaviour
 {
+    static FirebaseFirestore db;
+    public GameManager gameManager;
+
+    //나중에 싱글톤으로 해야한다
+    public void Init()
+    {
+        // Firebase Firestore 초기화
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            db = FirebaseFirestore.DefaultInstance;
+            Debug.Log("Firebase Firestore Initialized");
+        });
+    }
+
+    public void SetFirestore(string key, int value)
+    {
+        // 저장할 데이터
+        Dictionary<string, object> user = new Dictionary<string, object>
+        {
+            { key, value }
+        };
+        /*
+            { key, value },
+            { key, value }
+        */
+        //일단 employees에 Employee가 들어있어야 한다
+
+        // "users" 컬렉션에 새로운 문서 생성
+        //db.Collection("user").AddAsync(user).ContinueWithOnMainThread(task =>
+        db.Collection("user").Document("milkan660").SetAsync(user).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("데이터 입력 성공");
+            }
+            else
+            {
+                Debug.LogError("Error adding data: " + task.Exception);
+            }
+        });
+    }
+
+    public async Task<object> GetFirestore(string id, string key)
+    {
+        Dictionary<string, object> result = null;
+        // 특정 문서 가져오기 => 임시 
+        DocumentReference docRef = db.Collection("user").Document(id);
+        await docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DocumentSnapshot snapshot = task.Result;
+                if (snapshot.Exists)
+                {
+                    result = snapshot.ConvertTo<Dictionary<string, object>>();
+                    Debug.Log("Get Data : " + result[key]);
+                    gameManager.Money = (int)result[key];
+                }
+                else
+                {
+                    Debug.Log("No such document!");
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to get document: " + task.Exception);
+            }
+        });
+
+
+        return result[key];
+    }
+
 }
 //mDatabaseRef.Child("users").Child(userId).Child("username").SetValueAsync(name);
