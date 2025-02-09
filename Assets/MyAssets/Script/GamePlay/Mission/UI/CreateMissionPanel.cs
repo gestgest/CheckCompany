d30b70bad284fb94343c7b54c26b84cecf704140
gestgest <87427692+gestgest@users.noreply.github.com>
2025-02-09 23:31:06 +0900

    move assets

---
+++ Assets/MyAssets/Script/GamePlay/Mission/UI/CreateMissionPanel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class CreateMissionPanel : Panel
+{
+    [SerializeField] private TMP_InputField title_InputField;
+    [SerializeField] private RadioButtonGroup radioGroup;
+    
+    
+}
