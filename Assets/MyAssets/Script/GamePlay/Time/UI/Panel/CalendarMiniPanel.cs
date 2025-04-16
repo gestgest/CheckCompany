7cc7244d5f03453448b8b91af5f0a9a0d9ed55da
gestgest <milkan660@naver.com>
2025-04-16 19:51:57 +0900

    implement CalendarMiniPanel

--- Assets/MyAssets/Script/GamePlay/Time/UI/Panel/CalendarMiniPanel.cs
+++ Assets/MyAssets/Script/GamePlay/Time/UI/Panel/CalendarMiniPanel.cs
@@ -1,6 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class CalendarMiniPanel : MiniPanel
 {
-    
+    [SerializeField] private TextMeshProUGUI year_text;
+    [SerializeField] private TextMeshProUGUI month_text;
+
+    private int year;
+    private int month;
+
+    void SetValue(int year, int month)
+    {
+        this.year = year;
+        this.month = month;
+
+        year_text.text = year.ToString();
+        month_text.text = month.ToString();
+
+        //CalendarPanel을 동기화?
+    }
+
 }
